15164e5048a9e02f65f0e31737c6441250d2cbbc
Marcel <info@kraas.media>
2023-05-12 20:13:42 +0200

    08 Handle A Form Submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -6,6 +6,13 @@ export default {
    <section class="space-y-6">
       <assignment-list :assignments="filters.inProgress" title="In Bearbeitung"></assignment-list>
       <assignment-list :assignments="filters.completed" title="Erledigt"></assignment-list>
+
+      <form @submit.prevent="add">
+         <div class="border border-gray-600 text-black">
+            <input v-model="newAssignment" placeholder="Neue Aufgabe..." class="p-2"/>
+            <button type="submit" class="bg-white p-2 border-l">Add</button>
+         </div>
+      </form>
    </section>
    
    `,
@@ -16,7 +23,9 @@ export default {
             { name: 'Projekt abschließen', complete: false, id: 1 },
             { name: 'Lese Kapitel 4', complete: false, id: 2 },
             { name: 'Hausaufgaben einreichen', complete: false, id: 3 },
-         ]
+         ],
+
+         newAssignment: ''
       }
    },
    computed: {
@@ -25,6 +34,17 @@ export default {
             inProgress: this.assignments.filter(assignement => ! assignement.complete),
             completed: this.assignments.filter(assignement => assignement.complete)
          }
+      }
+   },
+   methods: {
+      add() {
+         this.assignments.push({
+            name: this.newAssignment,
+            complete: false,
+            id: this.assignments.lenght + 1
+         });
+
+         this.newAssignment = ''
       }
    }
 }
