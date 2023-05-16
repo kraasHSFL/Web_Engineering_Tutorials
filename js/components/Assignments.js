4b36e00d092ff7ed3ef39bef21cc845841997686
Marcel <info@kraas.media>
2023-05-16 13:57:33 +0200

    09 Parent-Child State Communication

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,18 +1,14 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
 
 export default {
-   components: { AssignmentList },
+   components: { AssignmentList, AssignmentCreate },
    template: `
    <section class="space-y-6">
       <assignment-list :assignments="filters.inProgress" title="In Bearbeitung"></assignment-list>
       <assignment-list :assignments="filters.completed" title="Erledigt"></assignment-list>
 
-      <form @submit.prevent="add">
-         <div class="border border-gray-600 text-black">
-            <input v-model="newAssignment" placeholder="Neue Aufgabe..." class="p-2"/>
-            <button type="submit" class="bg-white p-2 border-l">Add</button>
-         </div>
-      </form>
+      <assignment-create @add="add"></assignment-create>
    </section>
    
    `,
@@ -24,8 +20,6 @@ export default {
             { name: 'Lese Kapitel 4', complete: false, id: 2 },
             { name: 'Hausaufgaben einreichen', complete: false, id: 3 },
          ],
-
-         newAssignment: ''
       }
    },
    computed: {
@@ -37,14 +31,12 @@ export default {
       }
    },
    methods: {
-      add() {
+      add(name) {
          this.assignments.push({
-            name: this.newAssignment,
+            name: name,
             complete: false,
             id: this.assignments.lenght + 1
          });
-
-         this.newAssignment = ''
       }
    }
 }
