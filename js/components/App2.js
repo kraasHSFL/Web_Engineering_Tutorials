d47c4896f609e63d914627cbfb795951f5286d0f
Marcel <info@kraas.media>
2023-05-17 10:39:54 +0200

    15 Named Slots

--- js/components/App2.js
+++ js/components/App2.js
@@ -1,8 +1,11 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-   components: { Assignments },
+   components: { Assignments, Panel },
    template: `
-      <assignments></assignments>
+      <div class="grid gap-6">
+         <assignments></assignments>
+      </div>
    `
 };
--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,10 +1,11 @@
 import Assignment from "./Assignment.js";
 import AssignmentTags from "./AssignmentTags.js";
+import Panel from "./Panel.js";
 
 export default {
-   components: { Assignment, AssignmentTags },
+   components: { Assignment, AssignmentTags, Panel },
    template: `
-      <section v-show="show && assignments.length" class="w-60">
+      <Panel v-show="show && assignments.length" class="w-60">
          <div class="flex justify-between item-start">
             <h2 class="font-bold mb-2">
             {{ title }}
@@ -27,7 +28,11 @@ export default {
             ></assignment>
          </ul>
          <slot></slot>
-      </section>
+
+         <template #footer>
+            Hier ist mein Footer
+         </template>
+      </Panel>
    `,
 
    props: {
