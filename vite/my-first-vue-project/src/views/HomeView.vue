2f9564fa418fdeb50f86769e88fd62efc317d866
Marcel <info@kraas.media>
2023-05-17 14:53:56 +0200

    18 Two Mental Leaps to Script Setup

--- vite/my-first-vue-project/src/views/HomeView.vue
+++ vite/my-first-vue-project/src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+  let message = $ref('Hello, World!');
+
+  let doSomething = () => {
+    alert("doing it now");
+  };
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <button @click="doSomething">Do Something</button>
   </main>
 </template>
